dbe0d0766ad1078de5f5256d8ab1b827d6f6771d
Leandro Ferreira <leandrolima_ferreira@hotmail.com>
2024-08-15 18:21:10 -0300

    feat: criando pagina history

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,57 @@
+import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import locale from 'date-fns/locale/pt-BR'
+import { HistoryContainer, HistoryList, Status } from './styles'
+import { CyclesContext } from '../../contexts/cyclesContext'
+
+export function History() {
+  const { cycles } = useContext(CyclesContext)
+
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Duração</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cycles.map((cycle) => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>
+                    {formatDistanceToNow(new Date(cycle.startDate), {
+                      addSuffix: true,
+                      locale: locale.ptBR,
+                    })}
+                  </td>
+                  <td>
+                    {cycle.finishedDate && (
+                      <Status statusColor="green">Concluído</Status>
+                    )}
+
+                    {cycle.interruptedDate && (
+                      <Status statusColor="red">Interrompido</Status>
+                    )}
+
+                    {!cycle.finishedDate && !cycle.interruptedDate && (
+                      <Status statusColor="yellow">Em andamento</Status>
+                    )}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
